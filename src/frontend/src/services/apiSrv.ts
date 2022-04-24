9ae14693da1db9e58b73c250137b4851838edb4a
Malik Rafsanjani <13520105@std.stei.itb.ac.id>
2022-04-24 18:55:21 +0700

    feat(frontend): enhance error handling alert

--- src/frontend/src/services/apiSrv.ts
+++ src/frontend/src/services/apiSrv.ts
@@ -41,9 +41,32 @@ class ApiSrv {
       });
       console.log(res);
       return res;
-    } catch (error) {
-      console.log(error);
-      alert(error);
+    } catch (error: any) {
+      console.log("JSON ERROR");
+      console.log(error.toJSON());
+      if (error.response) {
+        console.log("RESPONSE ERROR");
+        console.log(error.response);
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+        alert(
+          `Got an error from server:\n
+          - Message: "${error.response.data.message}" 
+          - Status: ${error.response.status}`
+        );
+      } else if (error.request) {
+        console.log("REQUEST ERROR");
+        alert(
+          `Got an error when trying to connect to server:\n
+          - Message: "${error.request}"\n
+          `
+        );
+        console.log(error.request);
+      } else {
+        alert("Got an unknown error with message: " + error.message);
+        console.log("Error", error.message);
+      }
       return null;
     }
   }
