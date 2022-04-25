d83baf0254220a879907902ccecc2d0cb8c92c34
rozanfa <rozanfadhilalhafidz@gmail.com>
2022-04-25 15:39:16 +0700

    Add similarity percentage return in BMMatch and KMPMatch

--- src/library/algorithm/bmMatch.go
+++ src/library/algorithm/bmMatch.go
@@ -23,42 +23,55 @@ func buildLast(pattern string) []int {
 	return last
 }
 
-func BMMatch(text, pattern string) int {
+func BMMatch(text, pattern string) (int, int) {
 	/* Return index of first match of pattern in text,
-	 * or -1 if no match
+	 * or -1 if no match and max length of matching substring
 	 *
 	 * Args :
 	 *    text : string to search in
 	 *    pattern : string to search for
 	 * Returns :
-	 *    int : index of first match of pattern in text, or -1 if no match */
+	 *    tuple (int, int) : 
+	 *        int : index of first match of pattern in text, or -1 if no match
+	 *		  int : max length of matching substring */
 
 	last := buildLast(pattern)
 	n := len(text)
 	m := len(pattern)
 	i := m - 1
-
-	/* Check if text is shorter than pattern */
-	if i > n-1 {
-		return -1
-	}
-
+	max_count := 0
 	j := m - 1
 
 	/* Loop through text until pattern is found */
 	for end := false; !end; end = (i > n-1) {
 		if pattern[j] == text[i] { /* Check if char matches */
 			if j == 0 {
-				return i
+				return i, m
 			} else {
 				i--
 				j--
 			}
 		} else { /* char not match */
+			max_count = utils.Max(max_count, m - j - 1)
 			lo := last[utils.GetDnaCode(text[i])]
 			i = i + m - utils.Min(j, 1+lo) /* Move i to next possible match */
+			
 			j = m - 1
 		}
 	}
-	return -1
+	
+	i = n - 1
+	j = m - 1
+	/* Check max length of last matching substring */
+	for end := false; !end; end = (i > n-1) {
+		if pattern[j] == text[i] { /* Check if char matches */
+			i--
+			j--
+		} else { /* char not match */
+			max_count = utils.Max(max_count, m - j - 1)
+			i = n
+		}
+	}
+
+	return -1, max_count
 }
--- src/library/algorithm/kmpMatch.go
+++ src/library/algorithm/kmpMatch.go
@@ -1,5 +1,7 @@
 package algorithm
 
+import "github.com/marfgold1/TubesStima3/src/library/utils"
+
 func computeFail(pattern string) []int {
 	/* Return array storing index of last
 	 * occurence of each DNA char in pattern
@@ -30,7 +32,7 @@ func computeFail(pattern string) []int {
 	return fail
 }
 
-func KMPMatch(text, pattern string) int {
+func KMPMatch(text, pattern string) (int, int) {
 	/* Return index of first match of pattern in text,
 	 * or -1 if no match
 	 *
@@ -38,26 +40,30 @@ func KMPMatch(text, pattern string) int {
 	 *    text : string to search in
 	 *    pattern : string to search for
 	 * Returns :
-	 *    int : index of first match of pattern in text, or -1 if no match */
+	 *    tuple (int, int) : 
+	 *        int : index of first match of pattern in text, or -1 if no match
+	 *		  int : mmax length of matching substring */
 	fail := computeFail(pattern)
 	n := len(text)
 	m := len(pattern)
 	i := 0
 	j := 0
+	max_count := 0
 
 	for end := false; !end; end = i >= n {
 		if pattern[j] == text[i] {
 			if j == m-1 {
-				return i - m + 1
+				return i - m + 1, m
 			} else {
 				i++
 				j++
 			}
 		} else if j > 0 {
+			max_count = utils.Max(max_count, j)
 			j = fail[j-1]
 		} else {
 			i++
 		}
 	}
-	return -1
+	return -1, max_count
 }
